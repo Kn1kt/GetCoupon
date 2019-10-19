a7c9c699c323357ae48181f26c1240e1f6c6b0c4
Kn1kt <47992492+Kn1kt@users.noreply.github.com>
2019-10-19 23:40:15 +0300

    Initial Commit

---
+++ GetCoupon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetCoupon
+//
+//  Created by Nikita Konashenko on 19.10.2019.
+//  Copyright © 2019 Nikita Konashenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
