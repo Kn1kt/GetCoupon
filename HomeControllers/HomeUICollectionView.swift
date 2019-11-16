fc52259c8c3137053d7d1493b08fd4e8d363753e
Kn1kt <47992492+Kn1kt@users.noreply.github.com>
2019-11-16 19:01:35 +0300

    setup initial interface

---
+++ HomeControllers/HomeUICollectionView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeUICollectionView.swift
+//  GetCoupon
+//
+//  Created by Nikita Konashenko on 23.10.2019.
+//  Copyright © 2019 Nikita Konashenko. All rights reserved.
+//
+
+import UIKit
+
+class HomeUICollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+}
