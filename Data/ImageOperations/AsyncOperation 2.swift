5d28a6f0c5bad9498efd8d3ab5733a18dccd0f47
Kn1kt <47992492+Kn1kt@users.noreply.github.com>
2020-01-20 21:20:55 +0300

    last commit from Xcode

---
+++ Data/ImageOperations/AsyncOperation 2.swift
@@ -0,0 +1,55 @@
+//
+//  AsyncOperation.swift
+//  GetCoupon
+//
+//  Created by Nikita Konashenko on 13.01.2020.
+//  Copyright © 2020 Nikita Konashenko. All rights reserved.
+//
+
+import Foundation
+
+class AsyncOperation: Operation {
+    
+  // Create state management
+  enum State: String {
+    case ready, executing, finished
+
+    fileprivate var keyPath: String {
+      return "is\(rawValue.capitalized)"
+    }
+  }
+
+  var state = State.ready {
+    willSet {
+      willChangeValue(forKey: newValue.keyPath)
+      willChangeValue(forKey: state.keyPath)
+    }
+    didSet {
+      didChangeValue(forKey: oldValue.keyPath)
+      didChangeValue(forKey: state.keyPath)
+    }
+  }
+
+  // Override properties
+  override var isReady: Bool {
+    return super.isReady && state == .ready
+  }
+
+  override var isExecuting: Bool {
+    return state == .executing
+  }
+
+  override var isFinished: Bool {
+    return state == .finished
+  }
+
+  override var isAsynchronous: Bool {
+    return true
+  }
+
+  // Override start
+  override func start() {
+    main()
+    state = .executing
+  }
+}
