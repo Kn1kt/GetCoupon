0adebf0c67f24601e8deeb0ab25db82adbd24e7e
Kn1kt <47992492+Kn1kt@users.noreply.github.com>
2020-07-04 19:45:55 +0300

    delete files

---
+++ SearchControllers/SuggestedSearchesViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SuggestedSearchesViewController.swift
+//  GetCoupon
+//
+//  Created by Nikita Konashenko on 26.05.2020.
+//  Copyright © 2020 Nikita Konashenko. All rights reserved.
+//
+
+import Foundation
